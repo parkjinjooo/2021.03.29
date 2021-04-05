c2d7138ad5a5a7d8a0b3970267f743cb2d4e03c6
parkjinjooo <wlswn2155@gmail.com>
2021-04-05 20:08:33 +0900

    2021/04/05

--- src/day0405/SutdaDeck.java
+++ src/day0405/SutdaDeck.java
@@ -1,12 +1,21 @@
 package day0405;
 
-public class SutdaDeck {
+class SutdaDeck {
     final int CARD_NUM = 20;
+    // SutdaCard를 위한 공간만 마련되어 있다.
+    // 여기에 인스턴스를 직접 담아야 한다.
     SutdaCard[] cards = new SutdaCard[CARD_NUM];
 
     SutdaDeck() {
-
+        // 초기화를 통해 값들을 지정하고 인스턴스를 만들어 값을 담아준다.
+        for (int i = 0; i < cards.length; i++) {
+            // num, boolean 을 새롭게 초기화 해줘야 된다.
+            int num = i % 10 + 1;
+            boolean isKwang = (i < 10) && (num == 1 || num == 3 || num == 8);
+            cards[i] = new SutdaCard(num, isKwang);
+        }
     }
+
 }
 
 class SutdaCard {
